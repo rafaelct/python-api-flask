8b49a704f91cb63451acf90a55ac41cfb856d985
Rafael Costa Teixeira <rafaelct@primeinformatica.com.br>
2020-11-13 12:06:49 -0300

    Ajuste colocando a string de conexao do postgresql como variavel de ambiente usada pelo Heroku

--- config/Config.py
+++ config/Config.py
@@ -1,3 +1,6 @@
+import os
 
-strDb = "host=postgresql_container dbname=store user=postgres password=admin"
+DATABASE_URL = os.environ['DATABASE_URL']
+strDb = DATABASE_URL
+#strDb = "host=postgresql_container dbname=store user=postgres password=admin"
 expiredToken = 60
