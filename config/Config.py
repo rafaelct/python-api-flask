85c2c2d085d5ab991153b1d06e73426541335398
Rafael Costa Teixeira <rafaelct@primeinformatica.com.br>
2020-11-17 11:12:41 -0300

    Ajuste de 1 para 5 minutos a expiração do token.

--- config/Config.py
+++ config/Config.py
@@ -3,4 +3,5 @@ import os
 DATABASE_URL = os.environ['DATABASE_URL']
 strDb = DATABASE_URL
 #strDb = "host=postgresql_container dbname=store user=postgres password=admin"
-expiredToken = 60
+#expiredToken = 60
+expiredToken = 300
